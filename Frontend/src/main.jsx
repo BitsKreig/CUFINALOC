5ff9b5532bbcd81b37515184ebdefe198619ff31
Animesh <animeshkumarpandey150@gmail.com>
2025-11-02 06:22:28 +0530

    Commit all changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,42 +1,25 @@
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/app" element={<App />} />
-          <Route path="/timetable" element={<Timetable />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-    </ThemeProvider>
-  </BrowserRouter>
-);
-=======
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/app" element={<App />} />
-          <Route path="/timetable" element={<Timetable />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-    </ThemeProvider>
-  </BrowserRouter>
-);
-=======
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/app" element={<App />} />
-        <Route path="/timetable" element={<Timetable />} />
-        <Route path="/register" element={<RegisterPage />} />
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./useTheme";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./pages/LandingPage";
+import App from "./pages/App";
+import Timetable from "./pages/TimeTable";
+import MainLayout from "./layouts/MainLayout";
+import "./index.css";
 
->>>>>>> 5b326d3db6f5c6628fb1241b2577c45200d10acf
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ThemeProvider>
+<Routes>
+  <Route element={<MainLayout />}>
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/landing" element={<LandingPage />} />
+    <Route path="/app" element={<App />} />
+    <Route path="/timetable" element={<Timetable />} />
+    <Route path="/register" element={<RegisterPage />} />
+  </Route>
       </Routes>
     </ThemeProvider>
   </BrowserRouter>
